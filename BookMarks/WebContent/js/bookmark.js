4be44a3b4a959b59ef38e729ca27f6a439510e85
sushil bharwani <sushil.java@gmail.com>
2015-05-26 17:47:31 +0530

    Angular JS Example Application - BookMarks Add|Edit|Delete|Categorize

---
+++ BookMarks/WebContent/js/bookmark.js
@@ -0,0 +1,43 @@
+angular.module('bookmarks',[])
+.controller('MainCtrl', function($scope) {
+	$scope.categories = ["Javascript","CSS","HTML","Angular","Others"];
+	$scope.bookmarkItems = [
+	{title:"Angular JS",id:"1",url:"https://angularjs.org",category:"Javascript"},
+	{title:"JQuery",id:"2",url:"http://www.jQuery.com",category:"Javascript"},
+	{title:"Bootstrap",id:"3",url:"http://www.bootstrap.com",category:"css"},
+	{title:"Gulp",id:"4",url:"http://www.gulp.com",category:"css"},
+	{title:"Grunt",id:"5",url:"http://www.grunt.com",category:"HTML"},
+	{title:"XSS",id:"6",url:"http://www.xss.com",category:"HTML"},
+	{title:"PHP",id:"7",url:"http://www.php.net",category:"Angular"},
+	{title:"JAVA",id:"8",url:"http://www.oracle.com",category:"Angular"},
+	{title:"MySQL",id:"9",url:"http://www.mysql.com",category:"Others"}
+	];
+	
+	$scope.currentCategory = '';
+	
+	function setCurrentCategory(category){
+			
+			$scope.bookmark = {
+				title:"",
+				url :"",
+				id:"",
+				category:""
+					
+			};
+			$scope.currentCategory = category;
+	}
+	
+	function addBookMarks(bookmark){
+		console.log(bookmark);
+		bookmark.title = bookmark.title;
+		bookmark.url = bookmark.url;
+		bookmark.id = $scope.bookmarkItems.length+1;
+		bookmark.category = $scope.currentCategory;
+		
+		$scope.bookmarkItems.push(bookmark);
+				
+	}
+	
+	$scope.setCurrentCategory = setCurrentCategory;
+	$scope.addBookMarks = addBookMarks;
+})
